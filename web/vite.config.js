317d425842ca443798902090e95bdfd698b5169d
Sam Ireland <mail@samireland.com>
2026-03-02 00:33:16 +0000

    Dockerise

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    host: "localhost",
+    port: process.env.PORT || 3000,
+    proxy: {
+      "/api": {
+        target: process.env.API_URL,
+      },
+    },
+  },
+});
