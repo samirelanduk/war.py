8474434961d7e8715e6cc5d9fecda40dc521f445
Sam Ireland <mail@samireland.com>
2026-03-03 12:10:06 +0000

    Tiles

--- api/maps/models.py
+++ api/maps/models.py
@@ -7,3 +7,22 @@ class Map(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class TileType(models.TextChoices):
+    GRASS    = "G", "Grass"
+    SEA      = "S", "Sea"
+    FOREST   = "F", "Forest"
+    MOUNTAIN = "M", "Mountain"
+    RIVER    = "V", "River"
+    ROAD     = "R", "Road"
+    BRIDGE   = "B", "Bridge"
+
+
+
+class Tile(models.Model):
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    type = models.CharField(max_length=1, choices=TileType.choices)
